4dc0d390597763efcf7c637af65422971a0dae68
Niket Patil <pniket7@gmail.com>
2023-11-09 14:53:29 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,5 +1,3 @@
-from dotenv import load_dotenv
-import os
 import openai
 import streamlit as st
 from utils import ChatSession
@@ -7,19 +5,22 @@ from utils import ChatSession
 def main():
     st.title('Financial Bank Advisor Chatbot')
 
-    # Access the OpenAI API key from Streamlit secrets
-    api_key = st.secrets["openai_api_key"]
-    openai.api_key = api_key
+    # Load the OpenAI API key from Streamlit secrets
+    openai.api_key = st.secrets["api_key"]
 
     # Initialize the AdvisorGPT.
     sessionAdvisor = ChatSession(gpt_name='Advisor')
 
     # Instruct GPT to become a financial advisor.
     sessionAdvisor.inject(
-        line="You are a financial advisor at a bank. Greet the user with this message: 'Hello! How can I assist you with your banking today? What are you trying to accomplish with your banking?' Start the conversation by inquiring about the user's financial goals. If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals. If the user doesn't mention specific goals initially, guide them to discuss their financial goals, including age, annual income, and risk appetite, but do not ask for these details right at the beginning of the conversation. Always prioritize answering the user's questions over gathering information. Do not recommend specific financial actions or portfolios until you have a clear understanding of the user's financial situation and goals. Always maintain a customer-focused approach. Say 'ok' if you understand.",
+        line="You are a financial advisor at a bank. Greet the user with this message: 'Hello! How can I assist you with your banking today? What are you trying to accomplish with your banking?' Start the conversation by inquiring about the user's financial goals. If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals. ",
         role="user"
     )
     sessionAdvisor.inject(line="Ok.", role="assistant")
+
+    # Start the conversation.
+    user_input = ''
+    advisor_response = sessionAdvisor.chat(user_input=user_input, verbose=False)
 
     # Create a Streamlit text input for user input with a unique key
     user_input = st.text_input("User:", key="user_input")
