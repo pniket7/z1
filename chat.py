d2011dfc528407498aac260d7a0d71250d3270ed
Niket Patil <pniket7@gmail.com>
2023-11-28 12:38:56 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -216,6 +216,10 @@ def main():
         # Add the user's message to the chat history
         st.session_state.chat_history.append({"role": "user", "content": user_input})
 
+        # Display "Bot is thinking..." message while bot generates response
+        thinking_message = st.empty()
+        thinking_message.text("Bot is thinking...")
+
         # Update the chat session with the user's input
         st.session_state.sessionAdvisor.chat(user_input=user_input, verbose=False)
 
@@ -225,7 +229,8 @@ def main():
         # Remove newlines and extra spaces from the response
         advisor_response = advisor_response.replace('\n', ' ').strip()
 
-        # Add the chatbot's response to the chat history
+        # Replace "Bot is thinking..." with bot's response
+        thinking_message.empty()
         st.session_state.chat_history.append({"role": "bot", "content": advisor_response})
 
         # Display the chat history including new messages
@@ -237,7 +242,7 @@ def main():
                 chat_messages += f'<div style="text-align: {alignment}; margin-bottom: 10px;"><span style="background-color: {role_color}; color: white; padding: 8px 12px; border-radius: 20px; display: inline-block; max-width: 70%;">{message["content"]}</span></div>'
         
         chat_container.markdown(f'<div style="border: 1px solid black; padding: 10px; height: 400px; overflow-y: scroll;">{chat_messages}</div>', unsafe_allow_html=True)
-
+    
     # Create a button to start a new conversation
     if st.button("New Chat"):
         # Clear the chat history to start a new conversation
