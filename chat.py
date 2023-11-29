aa494fa1987e79fb42a77224b73da1e1eb91fc41
Niket Patil <pniket7@gmail.com>
2023-11-29 12:47:05 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -175,7 +175,7 @@ def update_investor_profile(session, investor_profile: dict, questions: list[str
 def initialize_sessionAdvisor():
     advisor = ChatSession(gpt_name='Advisor')
     advisor.inject(
-        line="You are a financial advisor at a bank. Start the conversation by inquiring about the user's financial goals. If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals. Be brief in your responses.",
+        line="You are a financial advisor at a bank. Start the conversation by inquiring about the user's financial goals. Follow up with each question based on the user's response, maintaining a sequential flow. Aim for a conversational approach, asking one question at a time and responding based on the received input. Avoid posing multiple questions simultaneously to ensure clarity and engagement.If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals. Be brief in your responses",
         role="user"
     )
     advisor.inject(line="Ok.", role="assistant")
