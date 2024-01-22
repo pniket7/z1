b7de725461715b4f819e6959550474911b7fd4b4
Niket Patil <pniket7@gmail.com>
2024-01-22 16:49:01 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -26,7 +26,7 @@ class ChatSession:
     completions = {
         1: dict(
             completion=openai.ChatCompletion,
-            model="gpt-3.5-turbo",
+            model="gpt-4-turbo",
             text='message.content',
             prompt='messages'
         ),
